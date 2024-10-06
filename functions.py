4aceedecd19d932bc852c6b12a3df8e7a7656a66
juan-b3ltran <beltranjuanjo.r@gmail.com>
2024-10-05 22:02:42 -0500

    app

--- functions.py
+++ functions.py
@@ -9,6 +9,10 @@ import numpy as np
 from scipy.fft import fft, ifft, fftfreq
 from scipy.signal import find_peaks
 from scipy import signal, ndimage
+import os
+import re
+import numpy as np
+from collections import defaultdict
 
 def data_extrac(N=27, data_directory = './data/lunar/training/data/S12_GradeA/'):
     '''
@@ -57,23 +61,59 @@ def data_extrac(N=27, data_directory = './data/lunar/training/data/S12_GradeA/')
 
 #---------------------------------------------------
 
-def data_extrac_test(N=1, data_directory = './data/lunar/test/data/S12_GradeB'):
+# def data_extrac_test(N=1, data_directory = './data/lunar/test/data/S12_GradeB'):
+#     '''
+#     Extrae los datos de un archivo N del catalogo y filtra por bandpass
+#     ademas mas cositas (proximamente)
+#     '''
+#     # Lista para almacenar los nombres de los archivos
+#     path_data = []
+
+#     # Recorrer todos los archivos en el directorio
+#     for nombre_archivo in os.listdir(data_directory):
+#         # Comprobar si es un archivo (y no una carpeta)
+#         if os.path.isfile(os.path.join(data_directory, nombre_archivo)) and nombre_archivo.endswith('.mseed'):
+#             path_data.append(nombre_archivo)
+
+#     print(f"Cantidad de datos en el directorio {data_directory} : {len(path_data)}")
+#     #Obtener datos de la señal
+#     mseed_file = data_directory+'/'+path_data[N]
+#     st = read(mseed_file)       #stream file (contains the trace(s))
+
+#     tr = st.traces[0].copy()    #Datos de la señal
+#     tr_times = tr.times()       #en segundos
+#     tr_data = tr.data
+
+#     #Filtro de band pass para frecuencias
+#     st_filt = st.copy()
+#     st_filt.filter('bandpass',freqmin=0.5,freqmax=1.0)
+#     tr_filt = st_filt.traces[0].copy()
+#     df = tr_filt.stats.sampling_rate
+#     tr_times_filt = tr_filt.times()
+#     tr_data_filt = tr_filt.data
+
+#     #Filtro de normalizacion de velocidades
+#     tr_data_filt_norm = tr_data_filt.reshape(-1, 1)
+#     scaler = MinMaxScaler(feature_range=(-1, 1))
+#     tr_data_filt_norm = scaler.fit_transform(tr_data_filt_norm)
+    
+#     #Filtro de normalizacion de tiempos
+#     tr_times_norm = tr_times.reshape(-1, 1)
+#     scaler = MinMaxScaler(feature_range=(0, 1))
+#     tr_times_norm = scaler.fit_transform(tr_times_norm)
+
+#     return tr_times, tr_data_filt_norm, df, tr_data,path_data[N]
+
+
+def data_extrac_test(filename):
+
     '''
     Extrae los datos de un archivo N del catalogo y filtra por bandpass
     ademas mas cositas (proximamente)
     '''
-    # Lista para almacenar los nombres de los archivos
-    path_data = []
 
-    # Recorrer todos los archivos en el directorio
-    for nombre_archivo in os.listdir(data_directory):
-        # Comprobar si es un archivo (y no una carpeta)
-        if os.path.isfile(os.path.join(data_directory, nombre_archivo)) and nombre_archivo.endswith('.mseed'):
-            path_data.append(nombre_archivo)
-
-    print(f"Cantidad de datos en el directorio {data_directory} : {len(path_data)}")
     #Obtener datos de la señal
-    mseed_file = data_directory+'/'+path_data[N]
+    mseed_file = filename
     st = read(mseed_file)       #stream file (contains the trace(s))
 
     tr = st.traces[0].copy()    #Datos de la señal
@@ -99,6 +139,7 @@ def data_extrac_test(N=1, data_directory = './data/lunar/test/data/S12_GradeB'):
     tr_times_norm = scaler.fit_transform(tr_times_norm)
 
     return tr_times, tr_data_filt_norm, df, tr_data
+
 
 def CFT(tr_data_filt_norm, df, sta_len = 60, lta_len = 900):
 
@@ -157,7 +198,7 @@ def peaks_plot(tr_times, filter_cft, prominence=0.3, distance=10000*6.6, wlen_va
         
         # Plot horizontal lines showing the left and right bases of each prominence
         ax.hlines(y=filter_cft[peak] - prominence, xmin=tr_times[left_base], xmax=tr_times[right_base], color="C1", linestyles="--")
-    ax.hlines(1, 0, tr_times[-1], color="gray", linestyles="--")
+    ax.hlines(height, 0, tr_times[-1], color="gray", linestyles="--")
     # Customize the plot
     ax.set_title(f"Peak Detection with Prominence: ({prominence}) (wlen={wlen_value}) Visualization")
     ax.legend()
@@ -199,7 +240,7 @@ def get_index_from_f(arrf,fval):
     index = np.where(aux<tol)
     return int(index[0])
 
-def hist_convolve_spectrogram(sxx, Nconvs):
+def hist_convolve_spectrogram(sxx, Nconvs, frequencies):
     conv = sxx/sxx.max()
     kernel = np.ones((10, 10))
     for i in range(Nconvs):
@@ -209,3 +250,58 @@ def hist_convolve_spectrogram(sxx, Nconvs):
     hist = np.sum(conv/conv.max(), axis=0)
     hist /= hist.max()
     return hist , conv
+
+def Signal_Plot(times,data,cft,id,filename):
+    fig,ax=plt.subplots(1,1,figsize=(12,3))
+    ax.plot(times,data,label=f'{id} test')
+    ax.plot(times, cft, label=f'CFT ')
+    ax.set_title(f'{filename}')
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('Velocity')
+    ax.legend()
+    plt.show()
+
+def extract_date_from_filename(filename):
+    # Expresión regular para buscar una fecha en el formato YYYY-MM-DD
+    match = re.search(r'\d{4}-\d{2}-\d{2}', filename)
+    if match:
+        return match.group(0)
+    return None
+
+def find_mseed_files_with_dates(root_dir):
+    # Recorrer todas las carpetas y archivos dentro del directorio raíz
+    dates_dict = {}
+    for foldername, subfolders, filenames in os.walk(root_dir):
+        for filename in filenames:
+            if filename.endswith('.mseed'):
+                filepath = os.path.join(foldername, filename)
+                # Extraer la fecha del nombre del archivo
+                date = extract_date_from_filename(filename)
+                if date in dates_dict:
+                        dates_dict[date].append(filepath)
+                else:
+                    dates_dict[date] = [filepath]
+
+    return dates_dict
+
+def dictionary_name(dir_name, dates_name_list):
+
+    for i, j in enumerate(dir_name):
+        root_directory = j
+        dates_name_list[i] = find_mseed_files_with_dates(root_directory)
+        
+
+    dict_name = defaultdict(list)
+
+    for d in dates_name_list:
+        for key, value in d.items():
+            dict_name[key].append(value)
+
+    dict_name = dict(dict_name)
+
+    dict_name = {k: dict_name[k] for k in sorted(dict_name.keys())}
+
+    # years = {fecha.split('-')[0] for fecha in dict_name.keys()}
+    # years = sorted(list(years))
+
+    return dict_name
